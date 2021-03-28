fa657c63156b3812a46520c8ae28112dfe9b906d
KIRILLAND78 <55920722+KIRILLAND78@users.noreply.github.com>
2021-03-28 13:03:57 +0300

    update

--- Items/Something.cs
+++ Items/Something.cs
@@ -1,7 +1,7 @@
 using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria;
-
+using Microsoft.Xna.Framework;
 
 namespace KirillandRandom.Items
 {
@@ -12,32 +12,72 @@ namespace KirillandRandom.Items
 			// DisplayName.SetDefault("Something"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
 			Tooltip.SetDefault("UGA-CHAGA!!!");
 		}
+        public override bool AltFunctionUse(Player player)
+        {
+			return true;
+		}
+        public override bool CanUseItem(Player player)
+        {
+			if (player.altFunctionUse == 2)
+			{
 
-		public override void SetDefaults()
+				item.useStyle = ItemUseStyleID.SwingThrow;
+				item.shoot = ProjectileID.ChargedBlasterOrb;
+				item.useTime = 30;
+				item.useAnimation = 30;
+
+				item.shootSpeed = 30;
+				item.knockBack = 1;
+				item.useStyle = ItemUseStyleID.Stabbing;
+				item.damage = 30;
+
+				item.UseSound = SoundID.DD2_SkyDragonsFuryShot;
+			}
+			else
+			{
+				
+				item.shoot = ProjectileID.None;
+				item.damage = 50;
+				item.melee = true;
+				item.width = 40;
+				item.height = 90;
+				item.useTime = 30;
+				item.useAnimation = 30;
+				item.useStyle = ItemUseStyleID.SwingThrow;
+				item.knockBack = 6;
+				item.value = 10000;
+				item.rare = ItemRarityID.Expert;
+				item.UseSound = SoundID.Item1;
+				item.autoReuse = true;
+			}
+			return true;
+        }
+        public override void SetDefaults()
 		{
 			item.damage = 50;
 			item.melee = true;
 			item.width = 40;
 			item.height = 90;
-			item.useTime = 20;
-			item.useAnimation = 20;
+			item.useTime = 30;
+			item.useAnimation = 30;
 			item.useStyle = ItemUseStyleID.SwingThrow;
 			item.knockBack = 6;
 			item.value = 10000;
-			item.rare = ItemRarityID.Expert;
+			item.rare = ItemRarityID.Expert; 
 			item.UseSound = SoundID.Item1;
 			item.autoReuse = true;
-			item.shoot = ProjectileID.ChargedBlasterOrb;
-			item.shootSpeed = 20;
 		}
-		//public override void CanEquipAccessory(Player player, int slot){		}
+
+
+		public override void OnHitNPC(Player player, NPC target, int damage, float knockBack, bool crit)
+		{
+			{
+				target.AddBuff(BuffID.CursedInferno, 30);//СИНИЙ ОГОНЬ СЮДА. ИЛИ ВООБЩЕ ЧТО-ТО ДРУГОЕ.
+			}
+		}
 
 
 
-
-
-
-		//public override void AltFunctionUse(Player player){		}
 		public override void AddRecipes()
 		{
 			ModRecipe recipe = new ModRecipe(mod);
@@ -46,10 +86,17 @@ namespace KirillandRandom.Items
 			recipe.SetResult(this);
 			recipe.AddRecipe();
 		}
-		public override void UpdateInventory(Player player)
-		{
-			//item.useTime = 32+Convert.ToInt32(item.TotalUseTime);
 
+
+
+		//КОД ДАЛЬШЕ Я ТУПО СПИЗД... позаимствовал с документации. и изменил немного. да, вот такой я гадкий.
+		public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+		{
+			Vector2 speed = new Vector2(speedX, speedY);
+			speed = speed.RotatedByRandom(MathHelper.ToRadians(5));
+			speedX = speed.X;
+			speedY = speed.Y;
+			return true;//это что-то типа коррекции поведения снарядов... наверное? По типу изменения урона, угла стрельбы и т.д.
 		}
 
 	}
